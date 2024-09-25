26cd7eff3c8ad7918e4bccc92d3201c1d002479f
shinshin273 <hongthamnguyen2703@gmail.com>
2024-09-25 18:13:29 +1000

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,7 @@
-import { expect } from 'chai';
+// app.test.js
+import { expect } from 'chai'; // Make sure this is the correct way of importing chai in your setup
 import request from 'supertest';
-import app from '../index.js'; 
+import app from '../index.js'; // Ensure this path is correct relative to your test file location
 
 describe('GET /', () => {
   it('should return 200 OK', async () => {
