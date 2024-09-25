fe60bab21b27687b829fd8b42479f599c561afbb
shinshin273 <hongthamnguyen2703@gmail.com>
2024-09-25 18:39:14 +1000

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,24 @@
-// app.test.js
-import { expect } from 'chai'; // Make sure this is the correct way of importing chai in your setup
+import { expect } from 'chai';
 import request from 'supertest';
-import app from '../index.js'; // Ensure this path is correct relative to your test file location
+import app from '../index.js';
 
 describe('GET /', () => {
+  let server;
+
+  // Start the server before running the tests
+  before(() => {
+    server = app.listen(3001, () => {
+      console.log('Test server running on port 3001');
+    });
+  });
+
+  // Close the server after tests are complete
+  after(() => {
+    server.close(() => {
+      console.log('Test server closed');
+    });
+  });
+
   it('should return 200 OK', async () => {
     const res = await request(app).get('/');
     expect(res.status).to.equal(200);
