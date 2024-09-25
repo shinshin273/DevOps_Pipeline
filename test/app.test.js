a44db3c1fddc50474c23107fee6a759b9b2a0116
shinshin273 <hongthamnguyen2703@gmail.com>
2024-09-25 18:43:29 +1000

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,26 +1,10 @@
-import { expect } from 'chai';
-import request from 'supertest';
-import app from '../index.js';
+import { expect } from 'chai'; 
+import request from 'supertest'; 
+import app from '../index.js'; 
 
 describe('GET /', () => {
-  let server;
-
-  // Start the server before running the tests
-  before(() => {
-    server = app.listen(3001, () => {
-      console.log('Test server running on port 3001');
-    });
-  });
-
-  // Close the server after tests are complete
-  after(() => {
-    server.close(() => {
-      console.log('Test server closed');
-    });
-  });
-
   it('should return 200 OK', async () => {
-    const res = await request(app).get('/');
+    const res = await request(app).get('/'); 
     expect(res.status).to.equal(200);
   });
 });
