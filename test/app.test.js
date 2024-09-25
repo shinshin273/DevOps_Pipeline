e3e17af1b80c76f76d096bc39b81eeabe4727431
shinshin273 <hongthamnguyen2703@gmail.com>
2024-09-25 21:04:54 +1000

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,20 @@
-const request = require('supertest'); 
-const { expect } = require('chai');
-const app = require('../index'); 
+const { expect } = require("chai");
+const converter = require("../app/converter");
 
-describe('GET /', () => {
-  it('should return 200 OK', async () => {
-    const res = await request(app).get('/'); 
-    expect(res.status).to.equal(200);
+describe("Ice Cream Sales Converter", function() {
+  describe("Dollars to Cents conversion", function() {
+    it("should convert dollars to cents correctly", function() {
+      expect(converter.dollarsToCents(1)).to.equal(100);
+      expect(converter.dollarsToCents(2.50)).to.equal(250);
+      expect(converter.dollarsToCents(0)).to.equal(0);
+    });
+  });
+
+  describe("Cents to Dollars conversion", function() {
+    it("should convert cents to dollars correctly", function() {
+      expect(converter.centsToDollars(100)).to.equal(1);
+      expect(converter.centsToDollars(250)).to.equal(2.50);
+      expect(converter.centsToDollars(0)).to.equal(0);
+    });
   });
 });
