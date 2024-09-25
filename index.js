721bcc970b57629b79703d369ee3f7c8990f4924
shinshin273 <hongthamnguyen2703@gmail.com>
2024-09-25 18:56:04 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-// Require the express web application framework 
-const express = require('express')
+// Require the express web application framework
+const express = require('express');
 
 // Create a new web application by calling the express function
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // Tell our application to serve all the files under the `public_html` directory
-app.use(express.static('public_html'))
+app.use(express.static('public_html'));
+
+// Define a route for the root URL to return a response
+app.get('/', (req, res) => {
+  res.status(200).send('Hi, Shin!'); // Ensure this route exists for your test
+});
 
 // Tell our application to listen to requests at port 3000 on the localhost
-app.listen(port, () => {
-  console.log(`Web server running at: http://localhost:${port}`)
-  console.log(`Type Ctrl+C to shut down the web server`)
-})
+const server = app.listen(port, () => {
+  console.log(`Web server running at: http://localhost:${port}`);
+  console.log(`Type Ctrl+C to shut down the web server`);
+});
+
+// Export the app for testing
+module.exports = app; 
